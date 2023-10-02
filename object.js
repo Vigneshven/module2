fd457ba632cc71c2b91a7dd01481f9e1be98588f
VIGNESH <130775921+Vigneshven@users.noreply.github.com>
2023-10-02 22:27:51 +0530

    Delete object.js

--- object.js
+++
@@ -1,7 +0,0 @@
-const person = {};
-person ["firstName"] = "Sidd";
-person["lastname"] = "verma";
-person.age = 24;
-person.designation = "fullstack developer";
-person.address = "up";
-console.log(person);
